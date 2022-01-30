bfaf4ed22fbc2045f63014c1f3fe7a32baac3917
Marco <marcomacchia99@gmail.com>
2022-01-30 22:53:24 +0100

    added instructions

--- Master/master.c
+++ Master/master.c
@@ -3,15 +3,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/time.h>
 #include <sys/resource.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <semaphore.h>
-#include <time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
