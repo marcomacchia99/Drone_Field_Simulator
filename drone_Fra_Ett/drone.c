125aac60a5bec0d3520cbe1e4471e8f63e06e4aa
Your Name <you@example.com>
2022-01-31 16:43:28 +0100

    Commenti

--- drone_Fra_Ett/drone.c
+++ drone_Fra_Ett/drone.c
@@ -13,8 +13,8 @@
 #include <sys/socket.h>
 #include <math.h>
 
-#define MAX_X 80     //max x value
-#define MAX_Y 40     //max y value
+#define MAX_X 80 // max x value
+#define MAX_Y 40 // max y value
 #define MAX_CHARGE 100
 
 #define RED '\033[1;31m'
@@ -24,9 +24,9 @@
 
 typedef struct drone_position_t
 {
-    //x position
+    // x position
     int x;
-    //y position
+    // y position
     int y;
 } drone_position;
 
@@ -34,22 +34,25 @@ typedef struct drone_position_t
 #define CHECK(X) ({int __val = (X); (__val == -1 ? ({fprintf(stderr,"ERROR (" __FILE__ ":%d) -- %s\n",__LINE__,strerror(errno)); exit(-1);-1;}) : __val); })
 
 /* GLOBAL VARIABLES */
-int command = 0;                                // Command received.
-// FILE *log_file;                                 // Log file.
-drone_position actual_position;
-drone_position next_position;
-drone_position landed;
+
+int command = 0; // Command received.
+// FILE *log_file;              // Log file.
+drone_position actual_position; // actual drone position
+drone_position next_position;   // next drone position
+drone_position landed;          // position for idle status
 int battery;
 
 /* FUNCTIONS HEADERS */
-float float_rand( float min, float max );
+float float_rand(float min, float max);
 // void logPrint ( char * string );
 void compute_next_position();
-void recharge( int newsockfd );
+void recharge(int newsockfd);
 
 /* FUNCTIONS */
-float float_rand( float min, float max ) {
-    /* Function to generate a randomic error. */
+float float_rand(float min, float max)
+{
+
+    /* Function to generate a randomic position. */
 
     float scale = rand() / (float)RAND_MAX;
     return min + scale * (max - min); // [min, max]
@@ -63,16 +66,20 @@ float float_rand( float min, float max ) {
 //     fflush(log_file);
 // }
 
-void compute_next_position() {
+void compute_next_position()
+{
+    /* Function to compute a new position. A better coverage algorithm will be implemented */
+
     next_position.x = (int)round(actual_position.x + float_rand(-1, 1));
     next_position.y = (int)round(actual_position.x + float_rand(-1, 1));
 }
 
-void recharge( int newsockfd ) {
+void recharge(int newsockfd)
+{
 
-    CHECK(write(newsockfd, &landed, sizeof(drone_position)));   // Writes command.
+    CHECK(write(newsockfd, &landed, sizeof(drone_position))); // Dummy command.
 
-    sleep(5);
+    sleep(5); // recharging time
 
     battery = MAX_CHARGE;
     printf("Battery fully charged. \n");
@@ -80,64 +87,60 @@ void recharge( int newsockfd ) {
 
 /* MAIN */
 
-int main() {
+int main()
+{
 
     printf("start \n");
     fflush(stdout);
 
-    int ret;                   //select() system call return value
-    char str[80];              // String to print on log file.
+    int ret; // this is the select() system call return value
+    // char str[80];              // String to print on log file.
 
-    int sockfd, newsockfd;                    // File descriptors.
-    int clilen, data;                         // Length of client address and variable for the read data.
-    int portno;                               // Used port number for socket connection.
-    struct sockaddr_in serv_addr, cli_addr;   // Address of the server and address of the client.
+    int sockfd, newsockfd;                  // File descriptors.
+    int clilen, data;                       // Length of client address and variable for the read data.
+    int portno = 8080;                      // Used port number.
+    struct sockaddr_in serv_addr, cli_addr; // Address of the server and address of the client.
 
+    // Initial Position
     actual_position.x = 10;
     actual_position.y = 10;
-    landed.x = MAX_X*2;
-    landed.y = MAX_Y*2;
+
+    // Dummy Position
+    landed.x = MAX_X * 2;
+    landed.y = MAX_Y * 2;
+
+    // Battery fully charged
     battery = MAX_CHARGE;
 
     // log_file = fopen("../log_file/Log.txt", "a"); // Open the log file.
-
     // logPrint("motor_x   : Motor x started.\n");
 
     /* Opens socket */
 
-    sockfd = CHECK(socket(AF_INET, SOCK_STREAM, 0));   // Creates a new socket.
+    sockfd = CHECK(socket(AF_INET, SOCK_STREAM, 0)); // Creates a new socket.
 
     printf("create socket \n");
     fflush(stdout);
 
     /* Initialize the serv_addr struct. */
-    bzero((char *)&serv_addr, sizeof(serv_addr));      // Sets all arguments of serv_addr to zero.
+    bzero((char *)&serv_addr, sizeof(serv_addr)); // Sets all arguments of serv_addr to zero.
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
+    serv_addr.sin_port = htons(portno);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
 
-    CHECK(connect(sockfd, &serv_addr, sizeof(serv_addr)));   // Listens on the socket for connections.
+    CHECK(connect(sockfd, &serv_addr, sizeof(serv_addr))); // Listens on the socket for connections.
 
     printf("connected \n");
     fflush(stdout);
 
-    // socklen_t len = sizeof(serv_addr);
-
-    // CHECK(getsockname(sockfd, (struct sockaddr *)&serv_addr, &len));   // Gets the port number chosen by the OS.
-
-    /* Prints on the terminal and on the log file the used port. */
-    // printf("Used port number: %d\n", ntohs(serv_addr.sin_port));
-    // sprintf(str, "Consumer Socket    :Used port number: %d \n", ntohs(serv_addr.sin_port));
-    // logPrint(str);
-
-    // portno = htons(serv_addr.sin_port);   // Stores the port number in network byte order.
-
-    while (1) {
+    while (1)
+    {
 
         // sprintf(str, "motor_x   : command received = %d.\n", command);
         // logPrint(str);
 
-        if (battery == 0) {
+        if (battery == 0)
+        { // Low battery
             printf("Low battery, landing for recharging. \n");
             fflush(stdout);
 
@@ -148,39 +151,41 @@ int main() {
         printf("next_x = %d, next_y =%d \n", next_position.x, next_position.y);
         fflush(stdout);
 
-        CHECK(write(newsockfd, &next_position, sizeof(drone_position)));   // Writes command.
+        CHECK(write(newsockfd, &next_position, sizeof(drone_position))); // Writes the next position.
 
         printf("written \n");
         fflush(stdout);
 
-        CHECK(read(newsockfd, &command, sizeof(int)));   // Reads command.
+        CHECK(read(newsockfd, &command, sizeof(int))); // Reads a feedback. ~ command = 0 if next_position is not allowed. ~ command = 1 if next posotion is allowed
 
-        printf("red \n");
+        printf("read \n");
         fflush(stdout);
-            
-        if (command == 0) {
+
+        // Not allowed
+        if (command == 0)
+        {
             printf("stay \n");
             fflush(stdout);
         }
-            
-        if (command == 1) {
+
+        // Allowed
+        if (command == 1)
+        {
             printf("let's move \n");
             fflush(stdout);
             actual_position = next_position;
         }
 
-        // est_pos_x = x_position + float_rand(-0.005, 0.005); //compute the estimated position
-
         // sprintf(str, "motor_x   : x_position = %f\n", x_position);
         // logPrint(str);
 
-        /* Sleeps. If the command does not change, repeats again the same command. */
+        // Battery decreases
         battery--;
-        sleep(1); //sleep for 1 second
+
+        // sleep for 1 second
+        sleep(1);
 
     } // End of the while cycle.
-
-    // logPrint("motor_x   : Motor x ended.\n");
 
     // fclose(log_file); // Close log file.
 
