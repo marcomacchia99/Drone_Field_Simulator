8f3d22febebac02b3430fb2dba2a387d3b5683a8
Ettore Sani <ettoresani0@gmail.com>
2022-02-13 19:41:03 +0100

    konsole PID

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ done
 
 echo "Port number $PORT selected" 
 
-konsole -p 'TerminalColumns=44' -p 'TerminalRows=84' -e "./src/dpm403_master/master $PORT" & echo "PID master        : $!"
+konsole -p 'TerminalColumns=44' -p 'TerminalRows=84' -e "./src/dpm403_master/master $PORT" & echo "PID shell of master        : $!"
 
 sleep 1
 
-konsole -e "./src/FE007_3_drone/drone $PORT" & echo "PID drone FE007   : $!"
-konsole -e "./src/al9_3_drone/drone $PORT" & echo "PID drone al9     : $!"
-konsole -e "./src/ML99_3_drone/drone $PORT" & echo "PID drone ML99    : $!"
-konsole -e "./src/drone_ale_fab/drone $PORT" & echo "PID drone ale_fab : $!"
+konsole -e "./src/FE007_3_drone/drone $PORT" & echo "PID shell of drone FE007   : $!"
+konsole -e "./src/al9_3_drone/drone $PORT" & echo "PID shell of drone al9     : $!"
+konsole -e "./src/ML99_3_drone/drone $PORT" & echo "PID shell of drone ML99    : $!"
+konsole -e "./src/drone_ale_fab/drone $PORT" & echo "PID shell of drone ale_fab : $!"
